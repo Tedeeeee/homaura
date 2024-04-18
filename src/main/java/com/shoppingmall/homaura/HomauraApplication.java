d9df37c664cfa9b43b85c64c20709baea17f0542
Tedeeee <gksrmf359@gmail.com>
2024-04-18 09:38:02 +0900

    setting

---
+++ src/main/java/com/shoppingmall/homaura/HomauraApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.homaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomauraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingmall/homaura/HomauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.homaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomauraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
